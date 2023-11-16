ca3a6d8c029cc3a91905da010b0e9030d6298fe9
Zeu Capua <zeucapua@gmail.com>
2023-11-15 21:06:07 -0800

    styling

--- src/routes/htmx-speedrun/+page.svelte
+++ src/routes/htmx-speedrun/+page.svelte
@@ -110,59 +110,66 @@
   $: current_time = active_cue_point?.time ?? 0;
 </script>
 
-<main class="flex gap-8 w-full h-full min-w-screen min-h-screen p-8">
-  <section class="flex flex-col gap-8 basis-1/2">
-    <div class="flex flex-col gap-4">
-      <a href="/" class="px-4 py-2 border rounded-full  w-fit">Back</a>
-      <h3 class="text-2xl font-bold">HTMX Delete Speedrun DEMO</h3>
-      <div class="flex gap-4 items-center">
-        <p>Chapter: {active_cue_point?.value.title}</p>
-        <select class="w-4 h-4" bind:value={active_cue_point} bind:this={chapter_selector}>
-          {#each cue_points as cue}
-            <option value={cue}>{cue.value.title}</option>
+<main class="flex flex-col gap-8 w-full h-full min-w-screen min-h-screen p-8 bg-zinc-800">
+  <div class="flex w-full h-full border border-2 border-zinc-700 rounded-xl">
+    <section class="flex flex-col gap-8 basis-1/2 bg-zinc-700 rounded-l-lg p-8 text-white">
+      <div class="flex flex-col gap-4">
+        <a href="/" class="px-4 py-2 border rounded-full  w-fit">Back</a>
+        <h3 class="text-2xl font-bold">HTMX Delete Speedrun DEMO</h3>
+        <div class="flex gap-4 items-center">
+          <p>Chapter: {active_cue_point?.value.title}</p>
+          <select 
+            class="w-4 h-4 rounded-lg text-black" 
+            bind:value={active_cue_point} 
+            bind:this={chapter_selector}
+          >
+            {#each cue_points as cue}
+              <option value={cue}>{cue.value.title}</option>
+            {/each}
+          </select>
+        </div>
+      </div>
+
+      <mux-player
+        bind:this={video_player}
+        currentTime={current_time}
+        playback-id="3OErv8jicBruoWo8Af7PLkSOIZ7ue9PhVHdcIjzUWRI"
+        metadata-video-id="video-id-54321"
+        metadata-video-title="HTMX Delete Speedrun"
+        metadata-viewer-user-id="user-id-123"
+        stream-type="on-demand"
+        accent-color="#FF0000"
+      ></mux-player>
+    </section>
+
+    <section class="basis-1/2 p-8 bg-white rounded-r-lg">
+      <div class="flex flex-col">
+        <div class="flex gap-4">
+          <label>
+            <input type="radio" bind:group={current_page} value="htmx-1" />
+            index.tsx
+          </label>
+
+          <label>
+            <input type="radio" bind:group={current_page} value="htmx-2" />
+            components.tsx
+          </label>
+        </div>
+
+        <div class="h-full max-h-96 font-mono overflow-y-auto">
+        {#each pages as page}
+            {#if page.id === current_page}
+              <Codepage 
+                step={active_cue_point?.value.step ?? 0} 
+                highlights={page.highlights}
+                code={page.content}
+              /> 
+            {/if}
           {/each}
-        </select>
+        </div>
       </div>
-    </div>
+    </section>
+  </div>
 
-    <div class="flex flex-col ">
-      <div class="flex gap-4">
-        <label>
-          <input type="radio" bind:group={current_page} value="htmx-1" />
-          index.tsx
-        </label>
-
-        <label>
-          <input type="radio" bind:group={current_page} value="htmx-2" />
-          components.tsx
-        </label>
-      </div>
-
-      <div class="">
-      {#each pages as page}
-          {#if page.id === current_page}
-            <Codepage 
-              step={active_cue_point?.value.step ?? 0} 
-              highlights={page.highlights}
-              code={page.content}
-            /> 
-          {/if}
-        {/each}
-      </div>
-    </div>
-
-  </section>
-
-  <section class="basis-1/2">
-    <mux-player
-      bind:this={video_player}
-      currentTime={current_time}
-      playback-id="3OErv8jicBruoWo8Af7PLkSOIZ7ue9PhVHdcIjzUWRI"
-      metadata-video-id="video-id-54321"
-      metadata-video-title="HTMX Delete Speedrun"
-      metadata-viewer-user-id="user-id-123"
-      stream-type="on-demand"
-      accent-color="#FF0000"
-    ></mux-player>
-  </section>
+  <p class="text-white text-center">demo by @zeu_dev {"<3"}</p>
 </main>
---
+++ src/routes/partykit/+page.svelte
@@ -0,0 +1,3 @@
+<main class="flex flex-col gap-8 w-full h-full min-w-screen min-h-screen p-8">
+  <a href="/" class="px-4 py-2 rounded-full w-fit border">Back</a>
+</main>
